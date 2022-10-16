61642a729b321539686b0dd5ddadcaf15b97acba
celiarodriguezmo <celiro02@gmail.com>
2022-10-16 19:26:56 +0200

    add constructor at services with local storage funtion

--- giftsApp/src/app/gifts/services/gifts.service.ts
+++ giftsApp/src/app/gifts/services/gifts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Gift, SearchGiftsResponse } from '../interface/gifts.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,28 @@ export class GiftsService {
 
     private _historial : string [] = [];
 
-    public apiResults : any []= [];
+    public apiResults : Gift []= [];
+
+    //damos el tipado a la data que viene de la api como : Gift[] esta información proviene de la interfaz que hemos creado//
 
     get historial () {
       
       return [...this._historial];
     }
 
-    constructor ( private http: HttpClient ) { }
+    constructor ( private http: HttpClient ) { 
+      
+      if ( localStorage.getItem ('historial') ) {
 
+        this._historial = JSON.parse ( localStorage.getItem('historial')! );
+
+      }
+      else{
+
+        this._historial = [];
+      }
+
+    }
 
     searchGifts (query : string = '') {
 
@@ -29,18 +43,24 @@ export class GiftsService {
 
         this._historial.unshift( query );
         this._historial = this._historial.splice(0,10);
+
+        //almacenar en local storage//
+
+        localStorage.setItem('historial' , JSON.stringify( this._historial) )
       }
 
 //Peticion al servidor tipo get usando el módulo de '@angular/common/http' llamado HttpClient que en lugar de promesas trabaja con obserbables y en lugar del .then() usa el .suscribe()//
 
 // para usarlo en el archivo services hemos tenido que importar este módulo en el módulo raiz HttpClientModule así '@angular/common/http'//
 
-      this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=DLqUEDhqC7IGuc1joNJRWXZlpOcDKQd7&q= ${ query } &limit=10`)
-      .subscribe( (resp : any) => {
+      //modificamos el tipado de la respuesta del get introduciendo <SearchGiftsresponse> que es lo que nos devuelve la interfaz creada para typescript de la data de la api//
+
+      this.http.get <SearchGiftsResponse> (`https://api.giphy.com/v1/gifs/search?api_key=DLqUEDhqC7IGuc1joNJRWXZlpOcDKQd7&q= ${ query } &limit=10`)
+      .subscribe( (resp) => {
 
         console.log( resp.data );
         this.apiResults = resp.data;
-
+        
       })
 
 // la manera tradicional de hacer la petición a la api sería esta//
